a5f37b6c107fe7de7b95a7ece3140cff1a5d45c4
Absar <siddiqui.mr10@gmail.com>
2024-01-31 08:28:47 +0530

    Login Functionality added in Backend

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,42 @@
 const mongoose = require("mongoose");
 const User = require("../models/userSchema");
 const hashPassword = require("../utils/hashedPassword");
+const comparePassword = require("../utils/comparePassword");
+const generateToken = require("../utils/generateToken");
 
 // login
 const login = async (req, res) => {
-  res.json({ msg: "login user" });
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400).json({ message: "All fields required" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(400).json({ message: "User Not registered" });
+    }
+
+    const passwordCheck = await comparePassword(password, user.password);
+
+    if (!passwordCheck) {
+      res.status(400).json({ message: "Incorrect Password" });
+    }
+
+    const token = await generateToken(user._id);
+
+    res.status(200).json({
+      message: "Login Successfull",
+      payload: token,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      message: "Internal Server Error Please try again or after sometime",
+    });
+  }
 };
 
 //signup
